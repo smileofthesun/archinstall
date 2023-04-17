bbb80742e67e0d603aca3f3a7aaf57eafbc1666e
smileofthesun <130895879+smileofthesun@users.noreply.github.com>
2023-04-17 08:42:19 +0200

    Update archinstall.sh

--- archinstall.sh
+++ archinstall.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
 
 sudo pacman -Syu --noconfirm
+
+if grep -q "Artix Linux" /etc/os-release; then
+    sudo pacman --noconfirm -S archlinux-keyring
+    echo "[universe]
+Server = https://universe.artixlinux.org/\$arch
+Server = https://mirror1.artixlinux.org/universe/\$arch
+Server = https://mirror.pascalpuffke.de/artix-universe/\$arch
+Server = https://artixlinux.qontinuum.space/artixlinux/universe/os/\$arch
+Server = https://mirror1.cl.netactuate.com/artix/universe/\$arch
+Server = https://ftp.crifo.org/artix-universe/" >>/etc/pacman.conf
+    pacman -Sy --noconfirm >/dev/null 2>&
+else
+    # Commands to run if the system is not Arch Linux
+    echo "This is not Arch Linux."
+fi
+
 #sudo pacman -S --noconfirm reflector rsync
 
 echo "Skip on Artix
@@ -12,37 +28,6 @@ if [ -n "$country" ]; then
 else
     echo "Skipping mirrorlist configuration..."
 fi
-
-refreshkeys() {
-	case "$(readlink -f /sbin/init)" in
-	*systemd*)
-		whiptail --infobox "Refreshing Arch Keyring..." 7 40
-		pacman --noconfirm -S archlinux-keyring >/dev/null 2>&1
-		;;
-	*)
-		whiptail --infobox "Enabling Arch Repositories for more a more extensive software collection..." 7 40
-		if ! grep -q "^\[universe\]" /etc/pacman.conf; then
-			echo "[universe]
-Server = https://universe.artixlinux.org/\$arch
-Server = https://mirror1.artixlinux.org/universe/\$arch
-Server = https://mirror.pascalpuffke.de/artix-universe/\$arch
-Server = https://artixlinux.qontinuum.space/artixlinux/universe/os/\$arch
-Server = https://mirror1.cl.netactuate.com/artix/universe/\$arch
-Server = https://ftp.crifo.org/artix-universe/" >>/etc/pacman.conf
-			pacman -Sy --noconfirm >/dev/null 2>&1
-		fi
-		pacman --noconfirm --needed -S \
-			artix-keyring artix-archlinux-support >/dev/null 2>&1
-		for repo in extra community; do
-			grep -q "^\[$repo\]" /etc/pacman.conf ||
-				echo "[$repo]
-Include = /etc/pacman.d/mirrorlist-arch" >>/etc/pacman.conf
-		done
-		pacman -Sy >/dev/null 2>&1
-		pacman-key --populate archlinux >/dev/null 2>&1
-		;;
-	esac
-}
 
 awk '!/^#/ {print $1}' packagelist | sudo pacman -S --noconfirm --needed -
 
